0f0e288a890a3b81c3cd84b1d82165fa9aa4886f
Dong Nan <raitodong@gmail.com>
2016-11-10 23:54:09 -0500

    modified

--- insight_testsuite/temp/src/Parser.java
+++ insight_testsuite/temp/src/Parser.java
@@ -13,12 +13,11 @@ public class Parser {
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private BufferedWriter backupWriter;
-    // Parser constructor, used for feature 1 case
+
     public Parser(BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
         this.bufferedReader = bufferedReader;
         this.bufferedWriter = bufferedWriter;
     }
-    // Overload the constructor, used for feature 2 & 3 case
     public Parser(BufferedReader bufferedReader, BufferedWriter bufferedWriter, BufferedWriter backupWriter) {
         this.bufferedReader = bufferedReader;
         this.bufferedWriter = bufferedWriter;
@@ -32,54 +31,42 @@ public class Parser {
 
         try {
             while ((line = bufferedReader.readLine()) != null) {
-                // Pass the title line, and any possible lines that are empty
                 if (line.startsWith("time") || line.isEmpty()) {
                     continue;
                 }
-                // Extract payer and receiver id
+
                 items = line.split(", ");
                 int id1 = Integer.parseInt(items[1]);
                 int id2 = Integer.parseInt(items[2]);
 
                 if (map.containsKey(id1)) {
-                    // If payer is already in the user map
                     User payer = map.get(id1);
                     ArrayList<Integer> adjacencyList = payer.getAdjacencyList();
-                    // If receiver is in payer's friend list,
-                    // write result "trusted" into output1 and parse next record
+
                     if (adjacencyList.contains(id2)) {
                         bufferedWriter.write("trusted");
                     }
-                    // If receiver is not in payer's friend list,
-                    // write result "unverified" into output1,
-                    // add receiver to payer's friend list and parse next record
                     else {
                         bufferedWriter.write("unverified");
                         payer.addIdToAdjacencyList(id2);
                         map.put(id1, payer);
                     }
                 }
-                // If payer is not yet in the user map,
-                // write result "unverified" into output1, add payer to user map
-                // and add receiver to payer's friend list
                 else {
                     bufferedWriter.write("unverified");
                     User payer = new User(id1);
                     payer.addIdToAdjacencyList(id2);
                     map.put(id1, payer);
                 }
-                // If receiver is already in the user map
+
                 if (map.containsKey(id2)) {
                     User receiver = map.get(id2);
                     ArrayList<Integer> adjacencyList = receiver.getAdjacencyList();
-                    // And payer is not in receiver's friend list,
-                    // add payer to receiver's friend list
+
                     if (!adjacencyList.contains(id1)) {
                         receiver.addIdToAdjacencyList(id1);
                     }
                 }
-                // If receiver is not in the user map,
-                // add receiver to the user map and add payer to receiver's friend list
                 else {
                     User receiver = new User(id2);
                     receiver.addIdToAdjacencyList(id1);
@@ -113,14 +100,10 @@ public class Parser {
                 int id2 = Integer.parseInt(items[2]);
 
                 // Feature 2 & 3
-
-                // Feature 2 is just to check is receiver is at least a friend of friend of payer, the idea is
-                    // 1. Check if receiver is in payer's friend list
-                    // 2. If not, check if receiver is in any of payer's friends' friend lists
                 User payer = map.get(id1);
 
                 ArrayList<Integer> adjacencyList = payer.getAdjacencyList();
-                // This HashSet is used for feature 3
+
                 HashSet<Integer> visited = new HashSet<Integer>();
                 visited.add(id1);
                 if (adjacencyList.contains(id2)) {
@@ -139,11 +122,6 @@ public class Parser {
                             continue outerloop;
                         }
                         // Feature 3
-                        // Feature 3 is to apply BFS to find if receiver is within 4th degree connection of payer
-                        // A HashSet is used to record visited users in the user map, avoid searching in infinite loops
-                        // Future work: If I have more time to work on this problem, I'll try to use a bi-directional BFS
-                        // to make the search more efficient, i.e. start BFS from the payer and receiver simultaneously,
-                        // and check if they have a same friend in the middle of network
                         visited.add(adjacency);
                         for (int friendAdjacency : friendAdjacencyList) {
                             if (visited.contains(friendAdjacency)) {
--- src/antifraud.java
+++ src/antifraud.java
@@ -12,15 +12,20 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class antifraud {
+    // Define BufferedReader and BufferedWriter to parse info from payment file and write result into output files
+    // bufferedReader is for reading payment info from batch_payment and stream_payment
+    // bufferedWriter is used to write results into output1 and output2
+    // backupWriter is to write results into output3
     private static BufferedReader bufferedReader;
     private static BufferedWriter bufferedWriter;
     private static BufferedWriter backupWriter;
 
     public static void main(String[] args) {
+        // Initialize bufferedReader, bufferedWriter and backupWriter as null
         bufferedReader = null;
         bufferedWriter = null;
         backupWriter = null;
-
+        // Check if argument is valid
         if (args.length < 5) {
             System.out.println("Command line: java antifraud <batch payment file name> <stream payment file name> <output1> <output2> <output3>");
             System.exit(0);
@@ -42,13 +47,14 @@ public class antifraud {
             System.err.println("Cannot create: " + args[2]);
             System.exit(1);
         }
-
+        // Create a HashMap object to store the all the users by <UserID, User> pair
+        // Feature 1 is to build this user graph and feature 2 & 3 use it
         HashMap<Integer, User> map = new HashMap<Integer, User>();
 
         Parser batchParser = new Parser(bufferedReader, bufferedWriter);
         batchParser.parseBatch(map);
 
-        // Create parser for feature 2
+        // Create parser for feature 2 & 3
         try {
             bufferedReader = new BufferedReader(new FileReader(args[1]));
         }
